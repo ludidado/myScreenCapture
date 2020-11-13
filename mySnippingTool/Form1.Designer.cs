eeb9c639f72eebb04aa8049665154e904aff52d3
Daniel Maurinac <ludidado@googlemail.com>
2020-11-13 21:25:41 +0100

    Initial comit

---
+++ mySnippingTool/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace mySnippingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pbScreenShot = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreenShot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Error;
+            this.notifyIcon1.BalloonTipText = "double click to open";
+            this.notifyIcon1.BalloonTipTitle = "mySnippingTool";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pbScreenShot
+            // 
+            this.pbScreenShot.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbScreenShot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbScreenShot.Location = new System.Drawing.Point(0, 0);
+            this.pbScreenShot.Name = "pbScreenShot";
+            this.pbScreenShot.Size = new System.Drawing.Size(800, 450);
+            this.pbScreenShot.TabIndex = 1;
+            this.pbScreenShot.TabStop = false;
+            this.pbScreenShot.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbScreenShot_MouseDown);
+            this.pbScreenShot.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbScreenShot_MouseMove);
+            this.pbScreenShot.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbScreenShot);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreenShot)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pbScreenShot;
+    }
+}
+
---
+++ mySnippingTool/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mySnippingTool
+{
+    public partial class Form1 : Form
+    {
+        // Hotkey Hook
+        KeyboardHook hook = new KeyboardHook();
+        //These variables control the mouse position
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+
+        //This variable control when you start the right click
+        bool start = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // show icon in Notification area
+            notifyIcon1.Visible = true;
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(mySnippingTool.ModifierKeys.Control,  Keys.F12); //  | mySnippingTool.ModifierKeys.Alt,
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            // MAGIC
+            //Create the Bitmap
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                     Screen.PrimaryScreen.Bounds.Height);
+
+            //Create the Graphic Variable with screen Dimensions
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            //Copy Image from the screen
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            //Create a temporal memory stream for the image
+            using (MemoryStream s = new MemoryStream())
+            {
+                //save graphic variable into memory
+                printscreen.Save(s, ImageFormat.Bmp);
+
+                pbScreenShot.Size = new System.Drawing.Size(this.Width, this.Height);
+                //set the picture box with temporary stream
+                pbScreenShot.Image = Image.FromStream(s);
+            }
+            //Show Form
+            this.Show();
+            //Cross Cursor
+            Cursor = Cursors.Cross;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Hide Form
+            this.Hide();
+
+        }
+
+        private void pbScreenShot_MouseMove(object sender, MouseEventArgs e)
+        {
+            //validate if there is an image
+            if (pbScreenShot.Image == null)
+                return;
+            //validate if right-click was trigger
+            if (start)
+            {
+                //refresh picture box
+                pbScreenShot.Refresh();
+                //set corner square to mouse coordinates
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                //draw dotted rectangle
+                pbScreenShot.CreateGraphics().DrawRectangle(selectPen,
+                          selectX, selectY, selectWidth, selectHeight);
+            }
+        }
+
+        private void pbScreenShot_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            //validate when user right-click
+            if (!start)
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    //starts coordinates for rectangle
+                    selectX = e.X;
+                    selectY = e.Y;
+                    selectPen = new Pen(Color.Red, 1);
+                    selectPen.DashStyle = DashStyle.DashDotDot;
+                }
+                //refresh picture box
+                pbScreenShot.Refresh();
+                //start control variable for draw rectangle
+                start = true;
+            }
+            else
+            {
+                //validate if there is image
+                if (pbScreenShot.Image == null)
+                    return;
+                //same functionality when mouse is over
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    pbScreenShot.Refresh();
+                    selectWidth = e.X - selectX;
+                    selectHeight = e.Y - selectY;
+                    pbScreenShot.CreateGraphics().DrawRectangle(selectPen, selectX,
+                             selectY, selectWidth, selectHeight);
+
+                }
+                start = false;
+                //function save image to clipboard
+                SaveToClipboard();
+            }
+        }
+        private void SaveToClipboard()
+        {
+            //validate if something selected
+            if (selectWidth > 0)
+            {
+                Rectangle rect = new Rectangle(selectX, selectY, selectWidth, selectHeight);
+                //create bitmap with original dimensions
+                Bitmap OriginalImage = new Bitmap(pbScreenShot.Image, pbScreenShot.Width, pbScreenShot.Height);
+                //create bitmap with selected dimensions
+                Bitmap _img = new Bitmap(selectWidth, selectHeight);
+                //create graphic variable
+                Graphics g = Graphics.FromImage(_img);
+                //set graphic attributes
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.DrawImage(OriginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+                //insert image stream into clipboard
+                Clipboard.SetImage(_img);
+            }
+            // hide Form
+            this.Hide();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //validate when user right-click
+            if (!start)
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    //starts coordinates for rectangle
+                    selectX = e.X;
+                    selectY = e.Y;
+                    selectPen = new Pen(Color.Red, 1);
+                    selectPen.DashStyle = DashStyle.DashDotDot;
+                }
+                //refresh picture box
+                pbScreenShot.Refresh();
+                //start control variable for draw rectangle
+                start = true;
+            }
+            else
+            {
+                //validate if there is image
+                if (pbScreenShot.Image == null)
+                    return;
+                //same functionality when mouse is over
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    pbScreenShot.Refresh();
+                    selectWidth = e.X - selectX;
+                    selectHeight = e.Y - selectY;
+                    pbScreenShot.CreateGraphics().DrawRectangle(selectPen, selectX,
+                             selectY, selectWidth, selectHeight);
+
+                }
+                start = false;
+                //function save image to clipboard
+                SaveToClipboard();
+            }
+        }
+    }
+
+
+
+    public sealed class KeyboardHook : IDisposable
+    {
+        // Registers a hot key with Windows.
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        // Unregisters the hot key with Windows.
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        /// <summary>
+        /// Represents the window that is used internally to get the messages.
+        /// </summary>
+        private class Window : NativeWindow, IDisposable
+        {
+            private static int WM_HOTKEY = 0x0312;
+
+            public Window()
+            {
+                // create the handle for the window.
+                this.CreateHandle(new CreateParams());
+            }
+
+            /// <summary>
+            /// Overridden to get the notifications.
+            /// </summary>
+            /// <param name="m"></param>
+            protected override void WndProc(ref Message m)
+            {
+                base.WndProc(ref m);
+
+                // check if we got a hot key pressed.
+                if (m.Msg == WM_HOTKEY)
+                {
+                    // get the keys.
+                    Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                    ModifierKeys modifier = (ModifierKeys)((int)m.LParam & 0xFFFF);
+
+                    // invoke the event to notify the parent.
+                    if (KeyPressed != null)
+                        KeyPressed(this, new KeyPressedEventArgs(modifier, key));
+                }
+            }
+
+            public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+            #region IDisposable Members
+
+            public void Dispose()
+            {
+                this.DestroyHandle();
+            }
+
+            #endregion
+        }
+
+        private Window _window = new Window();
+        private int _currentId;
+
+        public KeyboardHook()
+        {
+            // register the event of the inner native window.
+            _window.KeyPressed += delegate (object sender, KeyPressedEventArgs args)
+            {
+                if (KeyPressed != null)
+                    KeyPressed(this, args);
+            };
+        }
+
+        /// <summary>
+        /// Registers a hot key in the system.
+        /// </summary>
+        /// <param name="modifier">The modifiers that are associated with the hot key.</param>
+        /// <param name="key">The key itself that is associated with the hot key.</param>
+        public void RegisterHotKey(ModifierKeys modifier, Keys key)
+        {
+            // increment the counter.
+            _currentId = _currentId + 1;
+
+            // register the hot key.
+            if (!RegisterHotKey(_window.Handle, _currentId, (uint)modifier, (uint)key))
+                throw new InvalidOperationException("Couldn’t register the hot key.");
+        }
+
+        /// <summary>
+        /// A hot key has been pressed.
+        /// </summary>
+        public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            // unregister all the registered hot keys.
+            for (int i = _currentId; i > 0; i--)
+            {
+                UnregisterHotKey(_window.Handle, i);
+            }
+
+            // dispose the inner native window.
+            _window.Dispose();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Event Args for the event that is fired after the hot key has been pressed.
+    /// </summary>
+    public class KeyPressedEventArgs : EventArgs
+    {
+        private ModifierKeys _modifier;
+        private Keys _key;
+
+        internal KeyPressedEventArgs(ModifierKeys modifier, Keys key)
+        {
+            _modifier = modifier;
+            _key = key;
+        }
+
+        public ModifierKeys Modifier
+        {
+            get { return _modifier; }
+        }
+
+        public Keys Key
+        {
+            get { return _key; }
+        }
+    }
+    /// <summary>
+    /// The enumeration of possible modifiers.
+    /// </summary>
+    [Flags]
+    public enum ModifierKeys : uint
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Win = 8
+    }
+
+}
